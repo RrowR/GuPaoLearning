c378831d6a993febe4c417707ebedef4e9c3d515
Memo <2415782224@qq.com>
2022-04-23 18:08:10 +0800

    链表结构

---
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/edu/LinkedListDemo.java
@@ -0,0 +1,27 @@
+package com.gupao.edu;
+
+import java.util.LinkedList;
+
+/**
+ * @author: Rrow
+ * @date: 2022/4/23 17:49
+ */
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        /*  Node节点代码
+            查询速度低，修改速度高
+        private static class Node<E> {
+            E item;
+            Node<E> next;
+            Node<E> prev;
+            前指针         元素对象        后指针
+            Node(Node<E> prev, E element, Node<E> next) {
+                this.item = element;a
+                this.next = next;
+                this.prev = prev;
+            }
+        }
+        * */
+        LinkedList<String> list = new LinkedList<>();
+    }
+}
