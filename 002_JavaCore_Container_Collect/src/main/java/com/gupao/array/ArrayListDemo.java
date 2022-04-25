572ceb861ef3a88fa27c98fb68e158eded11eee8
Memo <2415782224@qq.com>
2022-04-26 00:58:21 +0800

    集合

---
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/array/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.gupao.array;
+
+import java.util.ArrayList;
+
+/**
+ * @author: Rrow
+ * @date: 2022/4/25 21:23
+ */
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list);
+        ArrayList list2 = new ArrayList(5);
+    }
+}
