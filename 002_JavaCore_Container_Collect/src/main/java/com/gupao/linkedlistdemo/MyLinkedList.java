24ace71ffc667ca710e193f0a98a0d6c8eaff47b
Memo <2415782224@qq.com>
2022-05-09 22:02:42 +0800

    linkedList

---
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/linkedlistdemo/MyLinkedList.java
@@ -0,0 +1,15 @@
+package com.gupao.linkedlistdemo;
+
+import java.util.LinkedList;
+
+/**
+ * @author: Rrow
+ * @date: 2022/5/8 13:12
+ */
+public class MyLinkedList {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.push(1);
+        list.add(1);
+    }
+}
