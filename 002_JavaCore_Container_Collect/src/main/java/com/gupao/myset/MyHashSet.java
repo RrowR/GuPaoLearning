7e0a93d426957c76f048b4c41dd615a7d58bbe31
Memo <2415782224@qq.com>
2022-05-11 01:05:57 +0800

    看到了treeset

---
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/myset/MyHashSet.java
@@ -0,0 +1,14 @@
+package com.gupao.myset;
+
+import java.util.HashSet;
+
+/**
+ * @author: Rrow
+ * @date: 2022/5/10 23:43
+ */
+public class MyHashSet {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+        hashSet.add(1);
+    }
+}
---
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/myset/MyTreeSet.java
@@ -0,0 +1,18 @@
+package com.gupao.myset;
+
+import java.util.TreeSet;
+
+/**
+ * @author: Rrow
+ * @date: 2022/5/11 0:11
+ */
+public class MyTreeSet {
+    public static void main(String[] args) {
+        /*
+        * 构造方法里有
+        *   this(new TreeMap<E,Object>());
+        * */
+        TreeSet set = new TreeSet();
+
+    }
+}
