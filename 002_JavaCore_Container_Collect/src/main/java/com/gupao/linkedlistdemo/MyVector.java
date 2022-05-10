2fa74e3710ebc933bb8bd9592d162e67b65466fe
Memo <2415782224@qq.com>
2022-05-10 23:42:35 +0800

    vector 的使用

--- 002_JavaCore_Container_Collect/src/main/java/com/gupao/linkedlistdemo/MyVector.java
+++ 002_JavaCore_Container_Collect/src/main/java/com/gupao/linkedlistdemo/MyVector.java
@@ -11,9 +11,10 @@ public class MyVector {
     private static List list = new ArrayList();
 
     public static void main(String[] args) {
-        // 里面的方法都是加锁的，所以都是数据安全的
+        // 里面的方法都是加锁的，所以都是数据安全的，速度慢，不推荐
         Vector<Object> vector = new Vector<>();
         vector.add(1);
+        // 让这个list变成一个带锁的，本质上是添加了同步代码块
         List synchronizedList = Collections.synchronizedList(list);
 
 
