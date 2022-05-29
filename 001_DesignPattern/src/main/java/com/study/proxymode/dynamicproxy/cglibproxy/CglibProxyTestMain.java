df732ab4263b5310eb080a8c174bf8313a4c6da8
Memo <2415782224@qq.com>
2022-05-30 02:17:03 +0800

    CGLIB代码生成

--- 001_DesignPattern/src/main/java/com/study/proxymode/dynamicproxy/cglibproxy/CglibProxyTestMain.java
+++ 001_DesignPattern/src/main/java/com/study/proxymode/dynamicproxy/cglibproxy/CglibProxyTestMain.java
@@ -1,12 +1,20 @@
 package com.study.proxymode.dynamicproxy.cglibproxy;
 
+import net.sf.cglib.core.DebuggingClassWriter;
+
 /**
  * @author: Rrow
  * @date: 2022/5/29 3:36
  */
 public class CglibProxyTestMain {
     public static void main(String[] args) {
-        Shuzuku shuzuku = (Shuzuku) new NekohaProxy().getInstance(Shuzuku.class);
-        shuzuku.play();
+
+        /*
+            这个要先放到上面才能生成出来，然后控制台会有显示
+            CGLIB debugging enabled, writing to 'F:\CGLIB'
+         */
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"./CGLIB");
+        new NekohaProxy().getInstance(Shuzuku.class);
+        // shuzuku.play();
     }
 }
