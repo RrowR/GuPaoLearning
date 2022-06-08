c8e326301d04aae5782e787e5916e34499bcc44d
Memo <2415782224@qq.com>
2022-06-08 20:18:58 +0800

    模板方法模式

---
+++ 001_DesignPattern/src/main/java/com/study/template/jdbc/Test.java
@@ -0,0 +1,16 @@
+package com.study.template.jdbc;
+
+
+import com.study.template.jdbc.dao.MemberDao;
+
+import java.util.List;
+
+/**
+ * Created by Tom.
+ */
+public class Test {
+    public static void main(String[] args) {
+        MemberDao memberDao = new MemberDao(null);
+        List<?> result = memberDao.selectAll();
+    }
+}
