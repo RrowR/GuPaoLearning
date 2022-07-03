d4148ade7f888200c799a120e23b9e509a6df186
Memo <2415782224@qq.com>
2022-07-04 00:31:47 +0800

    JUC

---
+++ JUC/src/main/java/com/juc/TestMain.java
@@ -0,0 +1,14 @@
+package com.juc;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author: Rrow
+ * @date: 2022/7/1 18:27
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+    }
+}
